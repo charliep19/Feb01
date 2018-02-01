d19b5b882515fae2206e866c517b70da8ef0385f
charliep19 <charliepenny33@outlook.com>
2018-02-01 14:21:49 -0500

    first

---
+++ F1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class F1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner= new Scanner(System.in);
+System.out.println("Enter");
+
+System.out.println("Enter Firstname");
+String FirstName= scanner.nextLine();
+
+System.out.println("Enter MiddleInitial");
+String MiddelInitial= scanner.nextLine();
+
+System.out.println("Enter LastName");
+String LastName=scanner.nextLine();
+
+System.out.println("Enter Career");
+String Career=scanner.nextLine();
+
+
+System.out.println("Enter Age");
+int Age=scanner.nextInt();
+
+	}
+
+}
---
+++ Human.java
@@ -0,0 +1,17 @@
+
+public class Human {
+	String FirstName;
+	String MiddleInitial;
+	String LastName;
+	String Career;
+	int YearsOld;
+public Human(String FirstName2, String MiddleInitial2, String Lastname2, String Career2, int YearsOld2){
+	FirstName=FirstName2;
+	MiddleInitial=MiddleInitial2;
+	LastName=Lastname2;
+	Career=Career2;
+	YearsOld=YearsOld2;
+
+ }	
+
+}
